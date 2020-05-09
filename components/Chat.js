77ac23c047a1c9c224c385409f3527e069a8d3e1
mfraggy25 <mfraggy25@users.noreply.github.com>
2020-05-09 13:41:07 -0400

    redo 4.3 due to git reset

--- components/Chat.js
+++ components/Chat.js
@@ -7,14 +7,72 @@ import {
   TextInput,
   ImageBackground,
 } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
+import KeyboardSpacer from "react-native-keyboard-spacer";
 
 export default class Chat extends React.Component {
-  //this will put the users name in navigation bar
+  constructor(props) {
+    super(props);
+  }
+  state = {
+    messages: [],
+  };
+
+  //Set navigation title as username instead of "Chat"
   static navigationOptions = ({ navigation }) => {
     return {
       title: navigation.state.params.name,
     };
   };
+
+  // Change bubble color
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          left: {
+            backgroundColor: "white",
+          },
+          right: {
+            backgroundColor: "#ECEFF1",
+          },
+        }}
+      />
+    );
+  }
+
+  // Set state with a static message
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello there!",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+        {
+          _id: 2,
+          text: "This is a system message",
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    });
+  }
+
+  // Send message function
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
   render() {
     return (
       <View
@@ -23,7 +81,14 @@ export default class Chat extends React.Component {
           { backgroundColor: this.props.navigation.state.params.color },
         ]}
       >
-        <Text style={{ color: "#FFFFFF" }}>This is your chat screen</Text>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {Platform.OS === "android" ? <KeyboardSpacer /> : null}
       </View>
     );
   }
@@ -31,9 +96,8 @@ export default class Chat extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    flex: 1, // Sets the width and height of the device
+    color: "#FFFFFF",
+    backgroundColor: "#000000",
   },
 });
